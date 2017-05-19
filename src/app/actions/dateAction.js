0b364bd4811ebe1733473a45f3a48c77cb22de4e
Pedro Felipe Froes <ac-pfroes@ac-pfroes-MAC.local>
2017-05-19 11:42:56 -0300

    Did you that Igor is a fucker???

---
+++ src/app/actions/dateAction.js
@@ -0,0 +1,5 @@
+export const getDate = () => {
+    return {
+        type: 'GET_DATE'
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider }	from 'react-redux';
+import index from './app/reducers/index';
 
 /* Import Pages */
 import App from './app/components/pages/App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
